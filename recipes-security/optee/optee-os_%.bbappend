YoeDistro/meta-ti

commit 280e7b4b3a171946c65581b2cfab4502d14cb99d

optee: update optee components to 3.20 tag
OPTEE tests is not being upgraded due to a newer python dependency for
the build

Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

recipes-security/optee/optee-os_%.bbappend
@@ -1,5 +1,5 @@
-PV_ti-soc = "3.19.0+git${SRCPV}"
-SRCREV_ti-soc = "d6c5d0037b46f46caf71d67d7825d4b722cbcddf"
+PV_ti-soc = "3.20.0+git${SRCPV}"
+SRCREV_ti-soc = "3bc3809afe372ca7e8216fc5d7a64e965bb4ad70"
 SRC_URI_ti-soc = "git://github.com/OP-TEE/optee_os.git;protocol=https"
 
 DEPENDS_append_ti-soc = " python3-cryptography-native"

recipes-security/optee/optee-client_%.bbappend
@@ -1,4 +1,7 @@
-PV_ti-soc = "3.19.0+git${SRCPV}"
-SRCREV_ti-soc = "140bf463046071d3ca5ebbde3fb21ee0854e1951"
+PV_ti-soc = "3.20.0+git${SRCPV}"
+SRCREV_ti-soc = "dd2d39b49975d2ada7870fe2b7f5a84d0d3860dc"
 
-DEPENDS_append_ti-soc = "pkgconfig util-linux"
+inherit pkgconfig
+DEPENDS_append_ti-soc = "util-linux"
+
+EXTRA_OEMAKE_append_ti-soc = " PKG_CONFIG=pkg-config"

recipes-security/optee/optee-examples_%.bbappend
@@ -1,5 +1,5 @@
-PV_ti-soc = "3.19.0+git${SRCPV}"
-SRCREV_ti-soc = "f301ee9df2129c0db683e726c91dc2cefe4cdb65"
+PV_ti-soc = "3.20.0+git${SRCPV}"
+SRCREV_ti-soc = "a98d01e1b9168eaed96bcd0bac0df67c44a81081"
 SRC_URI_ti-soc = "git://github.com/linaro-swg/optee_examples.git;protocol=https"
 
 DEPENDS_append_ti-soc = " python3-cryptography-native"

commit bf8c55cf0d2015c74a4b25f133395f4ce7a279de

optee: fix the sha for 3.20 tag
The SHA for optee_os had been of 3.20-rc1 tag instead of 3.20 tag which
had been marked stable. Change the tag to 3.20.

Fixes: 280e7b4b3a17 ("optee: update optee components to 3.20 tag")

Signed-off-by: Manorit Chawdhry <m-chawdhry@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

recipes-security/optee/optee-os_%.bbappend
@@ -1,5 +1,5 @@
 PV_ti-soc = "3.20.0+git${SRCPV}"
-SRCREV_ti-soc = "3bc3809afe372ca7e8216fc5d7a64e965bb4ad70"
+SRCREV_ti-soc = "8e74d47616a20eaa23ca692f4bbbf917a236ed94"
 SRC_URI_ti-soc = "git://github.com/OP-TEE/optee_os.git;protocol=https"
 
 DEPENDS_append_ti-soc = " python3-cryptography-native"