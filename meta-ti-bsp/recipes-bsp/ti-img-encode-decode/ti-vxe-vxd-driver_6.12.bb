YoeDistro/meta-ti

commit 296344c8ea44b958269352cd5a417adfd9df9857

ti-bsp: ti-img-encode-decode: Add Recipe for Building VXE-VXD Out-of-Tree Module
During 10.0 LTS bringup, the decision was made that it would be the last LTS
that carried the IMG VXE and VXD driver in tree. Driver is now hosted on
git.ti.com. This recipe is responsible for pulling in the driver,
building it, and packaging it within the J721e SDK.

Signed-off-by: Brandon Brnich <b-brnich@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-ti-bsp/recipes-bsp/ti-img-encode-decode/ti-vxe-vxd-driver_6.12.bb
@@ -1,0 +1,23 @@
+DESCRIPTION =  "Kernel drivers for the VXE384 and D5500 Video Accelerators found in the J721E TI SoC"
+LICENSE = "GPL-2.0-only"
+LIC_FILES_CHKSUM = "file://README;beginline=11;endline=12;md5=ad10b552e357ac443ec5dd2e02912b1b"
+
+inherit module
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+COMPATIBLE_MACHINE = "j721e"
+
+DEPENDS += "virtual/kernel"
+
+BRANCH = "scarthgap/k${PV}"
+
+SRC_URI = "git://git.ti.com/multimedia/img-vxe-vxd.git;protocol=https;branch=${BRANCH}"
+
+SRCREV = "99d063aa0ef53633057042570411b84bd967624b"
+
+TARGET_PRODUCT:j721e = "j721e_linux"
+
+S = "${WORKDIR}/git"
+
+EXTRA_OEMAKE += 'KERNEL_DIR="${STAGING_KERNEL_DIR}" PREFIX="${D}"'

commit 9cc9b341e63531b24d4c39a5e004c75da0051285

ti-bsp: ti-img-encode-decode: Out-of-Tree Recipe Cleanup
Module recipe is missing SUMMARY and a proper SRC_URI leading to
warnings during the build process.

Fixes: 296344c8 ("ti-bsp: ti-img-encode-decode: Add Recipe for Building VXE-VXD Out-of-Tree Module")
Reported-by: Denys Dmytriyenko <denys@konsulko.com>
Signed-off-by: Brandon Brnich <b-brnich@ti.com>
Signed-off-by: Ryan Eatmon <reatmon@ti.com>

meta-ti-bsp/recipes-bsp/ti-img-encode-decode/ti-vxe-vxd-driver_6.12.bb
@@ -1,4 +1,4 @@
-DESCRIPTION =  "Kernel drivers for the VXE384 and D5500 Video Accelerators found in the J721E TI SoC"
+SUMMARY =  "Kernel drivers for Video Accelerators found in the J721E TI SoC"
 LICENSE = "GPL-2.0-only"
 LIC_FILES_CHKSUM = "file://README;beginline=11;endline=12;md5=ad10b552e357ac443ec5dd2e02912b1b"
 
@@ -12,7 +12,7 @@ DEPENDS += "virtual/kernel"
 
 BRANCH = "scarthgap/k${PV}"
 
-SRC_URI = "git://git.ti.com/multimedia/img-vxe-vxd.git;protocol=https;branch=${BRANCH}"
+SRC_URI = "git://git.ti.com/git/multimedia/img-vxe-vxd.git;protocol=https;branch=${BRANCH}"
 
 SRCREV = "99d063aa0ef53633057042570411b84bd967624b"
 